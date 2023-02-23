a2730c1bd9c5085fc1012e9efe46e4afa59f79fc
damien.morard <damien.morard@unige.ch>
2023-02-23 16:26:17 +0100

    Finish to add tests for all cases of CTL

--- Tests/PredicateStructureTests/CTLTests.swift
+++ Tests/PredicateStructureTests/CTLTests.swift
@@ -180,38 +180,53 @@ final class CTLTests: XCTestCase {
     XCTAssertEqual(simpliedSPS3, expectedRes)
   }
   
-  func testForMe() {
-//    enum P: Place {
-//      typealias Content = Int
-//
-//      case p0,p1,p2
-//    }
-//
-//    enum T: Transition {
-//      case t0
-//    }
-//    
-//    let pn = PetriNet<P, T>(
-//      .pre(from: .p0, to: .t0, labeled: 1),
-//      .post(from: .t0, to: .p1, labeled: 1),
-//      capacity: [.p0: 3, .p1: 3, .p2: 3]
-//    )
-//    
-//    let ps: PS<P,T> = .ps([Marking([.p0: 0, .p1: 1, .p2: 0])], [Marking([.p0: 1, .p1: 1, .p2: 0]), Marking([.p0: 0, .p1: 2, .p2: 0]), Marking([.p0: 0, .p1: 1, .p2: 1])])
-//    
-//    print(ps.underlyingMarkings(petrinet: pn).count)
-//    
-//    let sps1: Set<PS<P,T>> = [.ps([Marking([.p0: 0, .p1: 1])], [])]
-//    let sps2: Set<PS<P,T>> = [
-//      .ps([Marking([.p0: 1, .p1: 2])], []),
-//      .ps([Marking([.p0: 0, .p1: 2])], [Marking([.p0: 0, .p1: 2])])
-//    ]
-//    print(PS.isIncluded(sps1: sps1, sps2: sps2))
-//
-//    resTemp [([[p0: 0, p1: 1]], [])
-//    ]
-//    res [([[p0: 1, p1: 2]], [])
-//    , ([[p0: 0, p1: 2]], [[p0: 1, p1: 2]])
+  func testCTLEval3() {
+    enum P: Place {
+      typealias Content = Int
+
+      case p0,p1,p2
+    }
+
+    enum T: Transition {
+      case t0,t1
+    }
+
+    // Following Petri net:
+    // p1
+    //       2 t1
+    //      <--
+    // p0 o     ▭ --> o p1
+    //    | -->
+    //    |  5
+    //    |
+    //    |
+    //     -->  ▭ --> o p2
+    //      7   t0
+    let pn = PetriNet<P, T>(
+      .pre(from: .p0, to: .t0, labeled: 7),
+      .post(from: .t0, to: .p2, labeled: 1),
+      .pre(from: .p0, to: .t1, labeled: 2),
+      .post(from: .t1, to: .p0, labeled: 5),
+      .post(from: .t1, to: .p1, labeled: 1)
+    )
+    
+    let ctlFormula1: CTL<P,T> = .AX(.and(.ap(.t1), .not(.ap(.t0))))
+    let ctlFormula2: CTL<P,T> = .EU(.ap(.t1), .ap(.t0))
+    let ctlFormula3: CTL<P,T> = .AU(.ap(.t1), .ap(.t0))
+    let sps1 = ctlFormula1.eval(petrinet: pn)
+    let simpliedSPS1 = PS.simplifiedSPS(sps: sps1)
+    let sps2 = ctlFormula2.eval(petrinet: pn)
+    let simpliedSPS2 = PS.simplifiedSPS(sps: sps2)
+    let sps3 = ctlFormula3.eval(petrinet: pn)
+    let simpliedSPS3 = PS.simplifiedSPS(sps: sps3)
+    
+    let ps1: PS<P,T> = .ps([Marking([.p0: 2, .p1: 0, .p2: 0])], [])
+    let ps2: PS<P,T> = .ps([Marking([.p0: 2, .p1: 0, .p2: 0])], [Marking([.p0: 4, .p1: 0, .p2: 0])])
+    let ps3: PS<P,T> = .ps([], [Marking([.p0: 2, .p1: 0, .p2: 0])])
+    
+    XCTAssertEqual(simpliedSPS1, [ps2,ps3])
+    XCTAssertEqual(simpliedSPS2, [ps1])
+    XCTAssertEqual(simpliedSPS3, [ps1])
   }
   
 }
